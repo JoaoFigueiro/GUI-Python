7a3b6a5935bdd63d0a05f67a3640119fd277a77f
JoaoFigueiro <joao.l.comiran@gmail.com>
2024-12-26 16:06:36 -0300

    feat: geometry and canvas

--- geometry.py
+++ geometry.py
@@ -23,6 +23,27 @@ def question():
     answer = messagebox.askyesno("?", "To be or not to be?")
     print(answer)
 
+def question_2():
+    answer = messagebox.askokcancel("?", "I'm going to format your hard drive")
+    print(answer)
+
+def question_3():
+    answer = messagebox.askretrycancel("?", "I'm going to format your hard drive")
+    print(answer)
+
+def question_4():
+    answer = messagebox.askquestion("?", "I'm going to format your hard drive")
+    print(answer)
+
+def question_5():
+    answer = messagebox.showerror("!", "Your code does nothing!")
+    print(answer)
+
+def question_6():
+    answer = messagebox.showwarning("Be careful!", "Big Brother is watching you!")
+    print(answer)
+
+
 size = 100
 grows = True
 window = tk.Tk()
@@ -35,6 +56,22 @@ window.bind("<Button-1>", click)
 window.protocol("WM_DELETE_WINDOW", really)
 
 button = tk.Button(window, text="Ask the question!", command=question)
+button.pack()
+
+button2 = tk.Button(window, text="Ask to Cancel!", command=question_2)
+button2.pack()
+
+button3 = tk.Button(window, text="Ask to Retry!", command=question_3)
+button3.pack()
+
+button4 = tk.Button(window, text="What are your plans?", command=question_4)
+button4.pack()
+
+button5 = tk.Button(window, text="Alarming message", command=question_5)
+button5.pack()
+
+button6 = tk.Button(window, text="What's going on?", command=question_6)
+button6.pack()
 
 
 window.mainloop()
